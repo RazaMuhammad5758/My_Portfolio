1753553b383ca3903108424e1d9335b07c6d0ceb
Raza Muhammad <waistband161@gmail.com>
2024-06-15 20:30:53 +0500

    changees

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ import Contact from './components/Contact'
 
 const App = () => {
   return (
-    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]'>
+
+    
+    <div className='overflow-x-hidden text-neutral-300 antialiased bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#000000_1px)] bg-[size:20px_20px]'>
       <div className='fixed top-0 -z-10 h-full w-full'></div>
       <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 "></div>
       
